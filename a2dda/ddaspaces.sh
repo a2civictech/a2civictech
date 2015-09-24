2fdb020a77e0bec9605196181061fd5512027dc3
Edward Vielmetti <edward.vielmetti@gmail.com>
2015-09-23 23:36:23 -0400

    initial edit of total systemwide

---
+++ a2dda/ddaspaces.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#
+# ddaspaces.sh - counts of spaces free in parking lots
+
+# initial version from @vielmetti provides a total count of spaces free systemwide
+
+CURL=/usr/bin/perl
+JQ=/usr/local/bin/jq
+COUNT=http://www.a2dda.org/map/AADDACount.json
+
+$CURL -s $COUNT | $JQ '[.countdata[] | (.spacesavail | tonumber)] | add'
